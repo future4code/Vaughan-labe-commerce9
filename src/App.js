f7b9c3136412edf05468dade12ac624d5a714cb3
graziellemcm <62907120+graziellemcm@users.noreply.github.com>
2022-01-21 17:56:13 -0300

    Revert "Pagina-de-Filtros"

--- src/App.js
+++ src/App.js
@@ -1,64 +1,26 @@
-import React, { Component } from "react"
-import jobList from "./data/jobs.json"
-import { Header } from "./components/Header";
-import { Card } from "./components/Card";
-import { ListContainer } from "./components/Filtros/styledFiltros";
-import { Filtros } from "./components/Filtros/Filtros";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export class App extends Component {
-
-   state = {
-      jobs: jobList,
-      busca: "",
-      valorMinimo: "",
-      valorMaximo: ""
-
-   }
-
-   atualizacaoDaBusca=(event) => {
-      this.setState ({
-         busca: event.target.value })
-   }
-
-   atualizacaoDoMinimo=(event) => {
-      this.setState ({
-         valorMinimo: event.target.value })
-   }
-
-   atualizacaoDoMaximo=(event) => {
-      this.setState ({
-         valorMaximo: event.target.value })
-   }
-
-   render() {
-      return <>
-        <Header/>
-         
-         <Filtros>
-            busca={this.state.busca}
-            atualizacaoDaBusca={this.atualizacaoDaBusca}
-            atualizacaoDoMinimo={this.atualizacaoDoMinimo}
-            atualizacaoDoMaximo={this.atualizacaoDoMaximo}
-            valorMinimo={this.state.valorMinimo}
-            valorMaximo={this.state.valorMaximo}
-         </Filtros>
-
-         <ListContainer>
-            {this.state.jobs
-            .filter(job => {
-               return job.title.toLowerCase().includes(this.state.busca.toLowerCase())
-            })
-            .filter(job => {
-               return this.state.valorMinimo === "" || job.price >= this.state.valorMinimo
-            })
-            .filter(job => {
-               return this.state.valorMaximo === "" || job.price <= this.state.valorMaximo
-            })
-            .map(job => {
-               return <Card key={job.id} job={job} />
-            })}
-         </ListContainer>
-      </>
-      
-   }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
